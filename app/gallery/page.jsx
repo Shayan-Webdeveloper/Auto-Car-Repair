d39ba5b7da767c0efdd009a34c49d2c978f67900
shayan-webdeveloper <shayanadec2@gmail.com>
2026-09-23 18:47:26 +0500

    Banner Completed

---
+++ app/gallery/page.jsx
@@ -0,0 +1,3 @@
+export default function Gallery() {
+     return(<h1 className="text-white">Gallery Page</h1>)
+}
---
+++ app/pricing/page.jsx
@@ -0,0 +1,3 @@
+export default function Pricing() {
+     return(<h1 className="text-white">Pricing Page</h1>)
+}
